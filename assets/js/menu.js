37e7f9c1b179f901b7827bfb88af2d9200dac56c
aarya-ch <62424000+aarya-ch@users.noreply.github.com>
2021-03-20 11:19:29 +0100

    Add files via upload

---
+++ assets/js/menu.js
@@ -0,0 +1,10 @@
+// menu.js
+
+function menuburger() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
